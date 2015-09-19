d2630cf2494d1a8bf172c00973ae3a317363078b
Miles Steele <miles@milessteele.com>
2015-09-19 13:07:32 -0400

    add android starter

--- android/parse-starter-project-1.10.2/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ android/parse-starter-project-1.10.2/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -12,6 +12,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 
@@ -25,12 +26,16 @@ public class StarterApplication extends Application {
     Parse.enableLocalDatastore(this);
 
     // Add your initialization code here
-    Parse.initialize(this);
+    Parse.initialize(this, "2fO5tL9EJyTk8FJKrCJzY5mr9lH9VI2T3cg3ZtHg", "Z8ukQzkYJjwp5Cpud0X2FYMAlFzRkLW56YPFfXze");
 
     ParseUser.enableAutomaticUser();
     ParseACL defaultACL = new ParseACL();
     // Optionally enable public read access.
     // defaultACL.setPublicReadAccess(true);
     ParseACL.setDefaultACL(defaultACL, true);
+
+    //ParseObject testObject = new ParseObject("TestObject");
+    //testObject.put("foo", "bar");
+    //testObject.saveInBackground();
   }
 }
