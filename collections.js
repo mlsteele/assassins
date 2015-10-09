683ee290551080445dd08204f13767af2e607aae
Miles Steele <miles@milessteele.com>
2015-10-09 19:55:32 -0400

    add schema validation

--- collections.js
+++ collections.js
@@ -1,47 +1,62 @@
 /*
-Database Collections: Players and Games
+Database collections and schemas.
 */
+
+Schemas = {};
+
 Games = new Mongo.Collection("games");
+
+Schemas.Game = new SimpleSchema({
+  id: {
+    type: "String",
+  },
+  mailingList: {
+    type: SimpleSchema.RegEx.Email,
+    label: "A single email to send annoucements to."
+  },
+  managerUserId: {
+    type: SimpleSchema.RegEx.Id,
+    label: "_id of the managing user."
+  },
+  started: {
+    type: "Boolean"
+  },
+  finished: {
+    type: "Boolean"
+  }
+});
+
+Games.attachSchema(Schemas.Game);
+
+
 Players = new Mongo.Collection("players");
 
-function load_sample_data() {
-  //sample user
-  Players.insert({
-    userId: "unknownAnna",
-    gameId: "Sample",
-    alive: true,
-    currentVictim: "JessId",
-    victimList: []
-  });
-  Players.insert({
-    userId: "unknownJess",
-    gameId: "Sample",
-    alive: true,
-    currentVictim: "AndresId",
-    victimList: []
-  });
-  Players.insert({
-    userId: "unknownAndres",
-    gameId: "Sample",
-    alive: false,
-    currentVictim: "MilesId",
-    victimList: []
-  });
+Schemas.Player = new SimpleSchema({
+  gameId: {
+    type: "String",
+    label: "id of the Game this player is in."
+  },
+  userId: {
+    type: SimpleSchema.RegEx.Id,
+    label: "_id of the User of this player."
+  },
+  currentVictim: {
+    type: SimpleSchema.RegEx.Id,
+    label: "_id of the current victim",
+    optional: true,
+  },
+  alive: {
+    type: "Boolean",
+    optional: true,
+  },
+  victimList: {
+    type: Array,
+    label: "_ids of players this one has killed.",
+    optional: true,
+  },
+  "victimList.$": {
+    type: SimpleSchema.RegEx.Id,
+  }
+});
 
-  Players.insert({
-    userId: "unknownMiles",
-    gameId: "Sample",
-    alive: false,
-    currentVictim: "AnnaId",
-    victimList: []
-  });
-
-  //sample game
-  Games.insert({
-    id: "Sample",
-    mailingList: "super_assassins@mit.edu",
-    managerUserId: "#(*$&#*$",
-    started: false,
-    finished: false
-  });
-}
+Players.attachSchema(Schemas.Player);
